fba5516028a142c716b13d4afccbe844019b2dfd
sridevi <sdara20@gmail.com>
2019-05-08 19:58:57 +0100

    Modify Appvue, GuestServices,GuestForm files

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,36 @@
 <template lang="html">
-  <guest-form/>
-  <guest-grid/>
-
+  <div id="app">
+   <guest-form/>
+   <guest-grid :bookings="bookings" />
+  </div>
 </template>
 
 <script>
 import GuestForm from '@/components/GuestForm.vue';
 import GuestGrid from '@/components/GuestGrid.vue';
+import { eventBus } from './main';
+import GuestService from './services/GuestService';
+
 export default {
   name: 'app',
+  data () {
+    return {
+      guests: []
+    }
+  },
   components: {
     GuestForm,
     GuestGrid
+  },
+  mounted() {
+    this.fetchData();
+    eventBus.$on('refresh-data', this.fetchData);
+  },
+  methods: {
+    fetchData(){
+      GuestService.getGuests()
+      .then(guests => this.guests = guests);
+    }
   }
 }
 </script>
