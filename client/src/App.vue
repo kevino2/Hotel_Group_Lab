d5990a4e49fda06a6f86bb7aab7a4de5b4ce4341
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-08 16:16:22 +0100

    Creates form template with v-models to update data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,17 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <guest-form/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GuestForm from '@/components/GuestForm.vue';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GuestForm
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ client/src/components/GuestForm.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <form class="" method="post">
+
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model="name" required/>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="email" required/>
+
+    <label for="status">Checked-in status:</label>
+    <input type="checkbox" id="status" v-model="status">
+
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      status: false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
+
+
+name
+email
+status
